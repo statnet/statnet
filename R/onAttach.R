19e2856dc88aba034ea4f5dc08073f32fdc2e188
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-03-21 20:24:54 +0000

    statnet package trunk now uses statnetStartupMessage().
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/trunk@11910 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,11 +1,5 @@
 .onAttach <- function(lib, pkg){
-  info <- packageDescription("statnet")
-  packageStartupMessage(
-  
-    paste('statnet: Version ', info$Version, ', created on ', info$Date, '\n',
-          'statnet is a suite of R packages for the statistical modeling of network data\n',
-          'See http://statnet.org for statnet project information', sep="")
-
- )
+  sm <- statnetStartupMessage("statnet", c(), TRUE)
+  if(!is.null(sm)) packageStartupMessage(sm)
 }
 
