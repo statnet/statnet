1260bbfa304aed3cb302760c30ecebd7ec483ab7
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-04-27 15:42:40 +0000

    * Created a 3.1 branch for statnet package. * networksis is now listed before tergm in the Depends: field. This way, simulate.network of tergm masks that of networksis (where it's an internal function), rather than the other way around.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/branches/3.1@11992 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,36 +1,5 @@
-
-# NB:  Package names can contain numbers but no statnet packages
-#      currently do.  The regexps used in the two strsplit calls
-#      below would need to be modified if they did.
-update_statnet <- function(...) {
-  # Extract the names of the packages to update.  First, the 
-  # packages in the Depends line of statnet/DESCRIPTION:
-  cat("Checking for newer versions of statnet and these other required packages:\n")
-  a <- packageDescription("statnet", fields="Depends")
-  b <- unlist( strsplit( a, '[^a-zA-Z.]+')) 
-  b <- b[b!="R" & b!="."]
-  cat(b, "\n\n")
-  update.packages(oldPkgs=c(b, "statnet"), ask=F, ...)
-  
-  # Next, the packages in the Suggests line of statnet/DESCRIPTION:
-  cat("Checking for newer versions of these suggested packages:\n")
-  a <- packageDescription("statnet", fields="Suggests")
-  if (!is.na(a)) {
-    b <- unlist( strsplit( a, '[^a-zA-Z.]+')) 
-    b <- b[b!="."]
-    cat(b, "\n\n")
-    update.packages(oldPkgs=b, ask="graphics", ...)
-#  cat("Type Y if you want each of the following suggested",
-#      "packages to be updated:\n")
-#  for (s in b) {
-#    cat(s)
-#    ans <- scan(what=character(), n=1, quiet=TRUE)
-#    if (length(ans)>0 && (ans=='Y' || ans=='y')) {
-#      update.packages(oldPkgs=s, ..., )
-#    }
-#  }
-  }
-  invisible(NULL)
+update_statnet <- function(...){
+  stop("This function has been deprecated. Use ", sQuote("update.packages()"),
+       " instead. For installing public beta releases from the Statnet Project ",
+       "reposititories, see http://www.statnet.org/preview .")
 }
-
-
