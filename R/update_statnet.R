89f6e8169abc087adb50e1d79eca48b78661fc36
Pavel N. Krivitsky <pavel@uow.edu.au>
2013-05-09 20:51:16 +0000

    * Made networksis, degreenet, and relevent Suggests rather than Depends. * update_statnet() is back, as a hard-coded wrapper around update.packages(), with setRepositories() used to optionally add a repository URL.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/branches/3.1@12013 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,5 +1,4 @@
-update_statnet <- function(...){
-  stop("This function has been deprecated. Use ", sQuote("update.packages()"),
-       " instead. For installing public beta releases from the Statnet Project ",
-       "reposititories, see http://www.statnet.org/preview .")
+update_statnet <- function(..., ask = FALSE, checkBuilt=TRUE, addURLs = character()){
+  if(length(addURLs)) setRepositories(addURLs = addURLs)
+  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent"), ask = ask, checkBuilt = checkBuilt, ...)
 }
