c2f5fc2e75e8702f8054406fe43be4759e69419e
Pavel N. Krivitsky <pavel@uow.edu.au>
2014-02-05 06:22:11 +0000

    Updated dependencies and version numbers in the trunk statnet.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/trunk@12577 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,4 +1,4 @@
 update_statnet <- function(..., ask = FALSE, checkBuilt=TRUE, addURLs = character()){
   if(length(addURLs)) setRepositories(addURLs = addURLs)
-  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent"), ask = ask, checkBuilt = checkBuilt, ...)
+  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent", "ndtv", "EpiModel"), ask = ask, checkBuilt = checkBuilt, ...)
 }
