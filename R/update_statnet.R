ac71e1be68abc3735f2335b3e4e276e9eac4ec09
Li Wang <lxwang@uw.edu>
2014-07-26 08:59:17 +0000

    Fixing #6 (Trac: 671). Adding a function to check for updates automatically. Details in ticket.
    git-svn-id: svn+ssh://statnet.csde.washington.edu/svn/statnet_commons/statnet/trunk@13200 9cb281f4-553f-406f-8d7f-11f072bbfe46

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,4 +1,32 @@
 update_statnet <- function(..., ask = FALSE, checkBuilt=TRUE, addURLs = character()){
   if(length(addURLs)) setRepositories(addURLs = addURLs)
-  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent", "ndtv", "EpiModel"), ask = ask, checkBuilt = checkBuilt, ...)
+  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent"), ask = ask, checkBuilt = checkBuilt, ...)
+  
+  message("Restart R and use \"update.packages(oldPkgs='statnet')\" to get the updates.")
+}
+
+
+check.updates <- function(show=T) {
+  stopifnot(require("tools")) ## load tools
+  ap <- installed.packages() 
+  deps <- package_dependencies('statnet', which=c('Depends','Imports','Suggests'), db = ap, recursive = F)[[1]]
+  deps <- c('statnet', deps)
+  
+  olds <- tryCatch({old.packages()},
+                   error = function(e) {
+                     message('unable to reach CRAN')
+                     NULL
+                   })
+  if (is.null(olds)) return(NULL)
+  
+  olds.statnet <- olds[rownames(olds) %in% deps, c(3,5,4), drop=FALSE]
+  if (show) {
+    if (nrow(olds.statnet) > 0) {
+      message("\nThere are updates for the following statnet packages on CRAN:")
+      print(olds.statnet)
+      message("Restart R and use \"update.packages(oldPkgs='statnet')\" to get the updates.")
+    }
+  } else {
+    return(olds.statnet)
+  }
 }
