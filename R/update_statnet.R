c0efa1df17b500420d1a79bf51c9d4c7aa397264
Pavel N. Krivitsky <pavel@uow.edu.au>
2018-10-17 13:27:35 +1100

    Added ergm.ego and ergm.rank to packages statnet pulls in and updates.

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,6 +1,6 @@
 update_statnet <- function(..., ask = FALSE, checkBuilt=TRUE, addURLs = character()){
   if(length(addURLs)) setRepositories(addURLs = addURLs)
-  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "latentnet", "networksis", "degreenet", "relevent"), ask = ask, checkBuilt = checkBuilt, ...)
+  update.packages(oldPkgs=c("statnet", "statnet.common", "network", "ergm", "sna", "networkDynamic", "tergm", "ergm.count", "ergm.rank", "latentnet", "networksis", "degreenet", "relevent", "ergm.ego"), ask = ask, checkBuilt = checkBuilt, ...)
   
 }
 
