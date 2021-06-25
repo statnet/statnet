34979b18a64a69a23d23986bea4dd9fd8ce255bc
Pavel N. Krivitsky <p.krivitsky@unsw.edu.au>
2021-06-25 11:21:14 +1000

    Converted the package to Roxygen.

--- R/update_statnet.R
+++ R/update_statnet.R
@@ -1,3 +1,39 @@
+#' Update the Component Packages of the Statnet Suite
+#' 
+#' A wrapper around \code{\link{update.packages}} to update the component
+#' packages of Statnet Suite to their latest versions.
+#' 
+#' 
+#' Updates the list component packages of Statnet Suite, using
+#' \code{\link{setRepositories}} and \code{\link{update.packages}}.
+#' 
+#' Since there are no good ways to update packages once they are loaded, this
+#' function should be called immediately after restarting R.
+#' 
+#' @param ask,checkBuilt Arguments to \code{\link{update.packages}}
+#' documentation. The defaults are different from those of that function.
+#' @param addURLs Optional repository URLs in addition to CRAN, such as
+#' \url{http://statnet.csde.washington.edu/preview}. Defaults to none.
+#' @param \dots Additional arguments to be passed to
+#' \code{\link{update.packages}}.
+#' @return \code{update_statnet} returns NULL invisibly.
+#' @seealso \code{\link{setRepositories}}, \code{\link{update.packages}},
+#' \code{\link{install.packages}}
+#' @keywords utilities
+#' @examples
+#' 
+#' \dontrun{
+#' # Update from CRAN
+#' statnet::update_statnet()
+#' 
+#' # Update from statnet.org's preview release, taking packages from CRAN
+#' # as needed
+#' statnet::update_statnet(addURLs="http://statnet.csde.washington.edu/preview")
+#' }
+#'
+#' @importFrom tools package_dependencies
+#' @importFrom utils installed.packages old.packages setRepositories update.packages
+#' @export
 update_statnet <- function(..., ask = FALSE, checkBuilt=TRUE, addURLs = character()){
   if(length(addURLs)) setRepositories(addURLs = addURLs)
   update.packages(oldPkgs=c("statnet", "statnet.common", "network", "sna", "ergm", 
